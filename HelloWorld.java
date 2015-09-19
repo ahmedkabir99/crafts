7769fe36c83a83c4ef527c1e69cbdd0fb9de4dfd
Ahmed Kabir <ahmedkabir99@gmail.com>
2015-09-19 12:33:54 +0200

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+
+import javax.swing.JOptionPane;
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		
+		String message; 
+		
+		message = "Hello World!!!";
+		
+		JOptionPane.showMessageDialog(null, message);
+		System.exit(0);
+		
+	}
+
+}
